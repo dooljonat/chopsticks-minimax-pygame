ef03502988cb59343b2e74a9396587db27d1f678
jdooley <jonathandool75@gmail.com>
2023-12-29 13:28:38 -0500

    Refactored code to be more modularized. Added enums for game descriptors

--- game.py
+++ game.py
@@ -1,17 +1,26 @@
 from random import randint
+from enum import Enum
+
+class GameState(Enum):
+    RUNNING = 0
+    DRAW = 1
+    GAME_OVER = 2
+
+class Player(Enum):
+    PLAYER = 1
+    COMPUTER = 2
 
 class Game:
     current_turn = 1
     current_board = [1,1,1,1]
 
-    player = 1
     player_indices = [0,1]
-
-    computer = 2
     computer_indices = [2,3]
 
-    def __init__(self):
-        pass
+    num_turns_resulting_in_draw = None    
+
+    def __init__(self, num_turns_resulting_in_draw=25):
+        self.num_turns_resulting_in_draw = num_turns_resulting_in_draw
 
     def print_hands(self, indices):
         _ = [1,2]
@@ -28,23 +37,35 @@ class Game:
         print("# \t" + self.print_hands(self.computer_indices))
         print("\n")
 
-    def check_state(self):
-        # Returns 1 if player lost
-        #           2 if computer lost
-        #           0 if game is still running
-        if self.current_board[0] == 0 and self.current_board[1] == 0:
-            return 1
-        elif self.current_board[2] == 0 and self.current_board[3] == 0:
-            return 2
+    def check_state(self, board, current_turn_number):
+        # If draw
+        if current_turn_number > self.num_turns_resulting_in_draw:
+            return GameState.DRAW
         else:
-            return 0
-        
-    def terminate_hands(self):
+            # If game was won
+            if (board[0] == 0 and board[1] == 0) or (board[2] == 0 and board[3] == 0):
+                return GameState.GAME_OVER
+            # If game running
+            else:
+                return GameState.RUNNING
+
+    def get_winner(self, board, current_turn_number):
+        if ((self.check_state(board, current_turn_number) == GameState.RUNNING)
+            or (self.check_state(board, current_turn_number) ==  GameState.DRAW)):
+            print("ERROR! get_winner() should only be called after check_state when game is terminated and not in a draw")
+        else:
+            if (board[0] == 0 and board[1] == 0):
+                return Player.COMPUTER
+            if (board[2] == 0 and board[3] == 0):
+                return Player.PLAYER
+
+    def terminate_hands(self, board):
         # Loops through game board,
         # Sets any index equal to zero if they hold a value > 5
-        for indx in range(len(self.current_board)):
-            if self.current_board[indx] >= 5:
-                self.current_board[indx] = 0
+        for indx in range(len(board)):
+            if board[indx] >= 5:
+                board[indx] = 0
+        return board
 
     def move_player(self):
         valid_move_made = False
@@ -63,10 +84,12 @@ class Game:
             player_hand_indx = int(player_hand_moved)-1
             opponent_hand_indx = (int(opponent_hand_picked)-1)+2
 
-            valid_move_made = self.update_board(player_hand_indx, opponent_hand_indx)
+            valid_move_made = self.is_valid_move(self.current_board, player_hand_indx, opponent_hand_indx)
 
             if not valid_move_made:
                 print("Please ensure you are picking a target hand that has not yet been elimnated! (e.g., value is 0)")
+            else:
+                self.current_board = self.update_board(self.current_board, player_hand_indx, opponent_hand_indx)
 
     def move_random_agent_computer(self):
         valid_move_made = False
@@ -75,89 +98,84 @@ class Game:
             computer_hand_moved = randint(2,3)
             player_hand_picked = randint(0,1)
         
-            valid_move_made = self.update_board(computer_hand_moved, player_hand_picked)
+            valid_move_made = self.is_valid_move(self.current_board, computer_hand_moved, player_hand_picked)
 
-#            if not valid_move_made:
-#                print("Please ensure you are picking a target hand that has not yet been elimnated! (e.g., value is 0)")
+            if valid_move_made:
+                self.current_board = self.update_board(self.current_board, computer_hand_moved, player_hand_picked)
 
     def move_minimax_agent_computer(self):
         pass
 
-    def update_board(self, indx, target_indx):
+    def is_valid_move(self, board, indx, target_indx):
+        return ((board[target_indx] > 0) 
+            and (board[indx] > 0) 
+            and (indx != target_indx) 
+            and (((indx in self.player_indices) and (target_indx in self.computer_indices)) or ((indx in self.computer_indices) or (target_indx in self.player_indices))))
+
+    def update_board(self, board, indx, target_indx):
         # Adds the value of the two indices together at the target index to
         # update the board after a move is made
-        # Will return false if an invalid move is made (target_indx is zero and the hand no longer exists)
-        if self.current_board[target_indx] > 0:
-            self.current_board[target_indx] += self.current_board[indx]
-            return True
-        else:
-            return False
+        board[target_indx] += board[indx]
+        return board
 
-    def split_hands(self, target_indices):
+    def is_valid_split(self, board, target_indices):
+        vals = [board[i] for i in target_indices]
+        vals.sort()
+        return ((vals[0] == 0)
+                and (vals[1] > 0)
+                and (vals[1] % 2 == 0))
+
+    def split_hands(self, board, target_indices):
         # Splits the values of the indices down the middle if
         # One index is eliminated (value is zero)
         # The other index is an even, divisible number
         # Returns True if successful and False if not
-        elim_indx = -1
-        split_indx = -1
+        vals = [board[i] for i in target_indices]
+        vals.sort()
+        new_val = vals[1]/2
         for indx in target_indices:
-            val = self.current_board[indx]
+            board[indx] = new_val
 
-            if val == 0:
-                elim_indx = indx
-            if val > 0:
-                if (val % 2) == 0:
-                    split_indx = indx
-
-        if ((elim_indx == -1) or (split_indx == -1) or (elim_indx == split_indx)):
-            return False
-        else:
-            split_val = current_board[split_indx] / 2
-
-            for _ in target_indices:
-                self.current_board[_] = split_val
-            return True
+        return board
     
 
     def play(self):
-        current_state = 0
-        current_player = self.player
+        current_state = GameState.RUNNING
+        current_player = Player.PLAYER
         current_turn = 1
 
         print("Welcome to finger chess game!\n")
 
-        while current_state == 0:
+        while current_state == GameState.RUNNING:
             print("Currently on turn: " + str(current_turn) + "\n")
             self.print_board()
 
             # Player's turn
-            if current_player == self.player:
+            if current_player == Player.PLAYER:
                 self.move_player()
 
                 # Switch player
-                current_player = self.computer
+                current_player = Player.COMPUTER
 
             # Computer's turn
-            elif current_player == self.computer:
+            elif current_player == Player.COMPUTER:
                 print("\tComputer is making their move....\n\n")
                 self.move_random_agent_computer()
                 
                 # Switch player
-                current_player = self.player
+                current_player = Player.PLAYER
 
             # Check state
-            self.terminate_hands()
-            current_state = self.check_state()
+            self.current_board = self.terminate_hands(self.current_board)
+            current_state = self.check_state(self.current_board, current_turn)
             
             current_turn += 1
 
-        if current_state == 2:
-            print("Player won!")
-        elif current_state == 1:
-            print("Computer won!")
-            
-
-
-            
-            
-            
+        if current_state == GameState.DRAW:
+            print("Game was a tie!")
+        else:
+            _ = self.get_winner(self.current_board, current_turn)
+            if _ == Player.PLAYER:
+                print("You won!")
+            if _ ==  Player.COMPUTER:
+                print("Computer won!")
--- mini_max_agent.py
+++ mini_max_agent.py
@@ -1,10 +1,64 @@
-
+import copy
+from game import Game
 
 class MiniMax:
-    def __init__(self, moves_to_look_forward_to=5):
-        self.move_to_look_forward_to = moves_to_look_forward_to
+    all_possible_moves = [["S"], [2, 0], [2, 1], [3, 0], [3, 1]]
 
-    def minimax(self, current_board, depth, isCurrentMaximizingAgent):
+    def __init__(self, game, moves_to_look_forward_to=5):
+        self.game = game 
+
+    def score(self, current_board, turn_number):
+        score = self.game.check_state(current_board, turn_number)
+        # Computer won
+        if score == 2:
+            return 10
+        # Player won
+        elif score == 1:
+            return -10
+        else:
+            return 0
+
+    def get_possible_moves(self, current_board):
+        possible_moves = copy.deepcopy(self.all_possible_moves)
+
+        indices_to_remove = []
+        # Check if split is available
+        if not (self.game.is_valid_split(current_board, self.game.computer_indices)):
+            indices_to_remove += [0]
+
+
+        # If either the target or move hand is terminated, the move can't happen, so remove it'
+        for i in range(1, len(possible_moves)):
+            move = possible_moves[i]
         
-        if current board is won or lost:
-            return state
+            for indx in move:
+                if current_board[indx] == 0:
+                    indices_to_remove += [i]
+
+        # Create new possible moveset
+        new_moves = []
+        for i in range(len(possible_moves)):
+            if i not in indices_to_remove:
+                new_moves += [possible_moves[i]]
+
+        # Return new moves
+        return new_moves
+
+    def minimax(self, current_board, current_turn, look_depth, isMaximizing):
+        score = self.score(current_board, current_turn+look_depth)
+
+        # If game has been running too long (Draw)
+
+        # If game was decided upon
+
+        # If game is still running
+
+        # If this is player's (minimizers) move
+
+g = Game()
+_ = MiniMax(g)
+g.current_board = [1,3,2,0]
+moves = _.get_possible_moves(g.current_board)
+print(moves)
+
+
